4c7466050b4809ed9729c3826434c057c1464f85
tcbalch <tcbalch@gmail.com>
2024-12-08 20:36:25 -0500

    Updated app.py and __init__.py to correctly register the books blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,21 @@
-from flask import Flask, render_template, session
+from flask import Flask, g
+from .app_factory import create_app
+from .db_connect import close_db, get_db
+
+app = create_app()
+app.secret_key = 'your-secret'  # Replace with an environment variable
+
+# Register Blueprints
 from app.blueprints.books import books_bp
-from app.db_connect import close_db
+app.register_blueprint(books_bp, url_prefix='/books')
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = 'your_secret_key'
+from . import routes
 
-    # Register Blueprints
-    app.register_blueprint(books_bp, url_prefix='/books')
+@app.before_request
+def before_request():
+    g.db = get_db()
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-
-    @app.route('/about')
-    def about():
-        return render_template('about.html')
-
-    @app.teardown_appcontext
-    def teardown_db(exception=None):
-        close_db(exception)
-
-    return app
+# Setup database connection teardown
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    close_db(exception)
